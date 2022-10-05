5a6543d9573a66f4a183f663103a22649c9fa262
xa4ipur4ik <kirill.nowoselow@mail.ru>
2022-10-05 13:38:48 +0300

    A lot of work.

--- app/src/main/java/com/example/lr1/MainActivity.java
+++ app/src/main/java/com/example/lr1/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.lr1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.lr1.Adapter.ToDoAdapter;
+import com.example.lr1.Model.ToDoModel;
+import com.example.lr1.Utils.DatabaseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DialogCloseListener {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+    private DatabaseHandler db;
+    private FloatingActionButton fab;
+
+    private List<ToDoModel> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(db, this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        fab = findViewById(R.id.fab);
+
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+
+//        ToDoModel task = new ToDoModel();
+//        task.setTask("This is a test from Iluha!");
+//        task.setStatus(0);
+//        task.setId(1);
+//
+////        taskList.add(task);
+////        taskList.add(task);
+////        taskList.add(task);
+////        taskList.add(task);
+////        taskList.add(task);
+////
+////        tasksAdapter.setTasks(taskList);
+    }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog) {
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
     }
 }
